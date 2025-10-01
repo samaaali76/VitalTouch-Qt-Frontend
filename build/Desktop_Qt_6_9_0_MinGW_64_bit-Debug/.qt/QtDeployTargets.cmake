eb4a7d8a85da84a23e96341ed911551459bfdb5b
samaaali-09 <692400448@ecu.edu.eg>
2025-10-01 21:32:09 +0300

    Remove build folder from repo (kept locally, ignored by .gitignore)

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_VitalTouch-Qt-Frontend_FILE C:/Users/HP/OneDrive - Egyptian Chinese University (ECU)/Desktop/VitalTouch-Qt-Frontend/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/VitalTouch-Qt-Frontend.exe)
-set(__QT_DEPLOY_TARGET_VitalTouch-Qt-Frontend_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_VitalTouch-Qt-Frontend_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
